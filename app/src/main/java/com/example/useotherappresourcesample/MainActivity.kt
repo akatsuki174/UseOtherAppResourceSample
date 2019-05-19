da5519930d10923eebaadb07be458a3f7145da19
akatsuki174 <dev2015js@gmail.com>
2019-05-19 13:35:43 +0900

    トーストで表示されるようにした

--- app/src/main/java/com/example/useotherappresourcesample/MainActivity.kt
+++ app/src/main/java/com/example/useotherappresourcesample/MainActivity.kt
@@ -5,28 +5,32 @@ import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        getOtherAppString()
     }
 
     private fun getOtherAppString() {
         val res: Resources?
         try {
-            res = this.packageManager.getResourcesForApplication(packageName)
             val packageName = "com.android.settings"
+            res = this.packageManager.getResourcesForApplication(packageName)
             val resourceId = res?.getIdentifier("$packageName:string/clear_activities", null, null)
-            if (resourceId != 0 && resourceId != null) {
+            if (resourceId != null) {
                 val text = this.packageManager.getText(packageName, resourceId, null)
-                Log.i("other_app_resource", "resource=$text")
+                Toast.makeText(this, text, Toast.LENGTH_LONG).show()
             }
         } catch (e: PackageManager.NameNotFoundException) {
             e.printStackTrace()
         }
+    }
+
+    fun onStringButtonClick(view: View) {
+        getOtherAppString()
     }
 }
