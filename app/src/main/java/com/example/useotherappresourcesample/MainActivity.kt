e2534fb0e776aca34d8306705587828516b35ac1
akatsuki174 <dev2015js@gmail.com>
2019-05-19 14:49:22 +0900

    他アプリの画像リソースをトースト表示するようにした

--- app/src/main/java/com/example/useotherappresourcesample/MainActivity.kt
+++ app/src/main/java/com/example/useotherappresourcesample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.useotherappresourcesample
 
 import android.content.pm.PackageManager
 import android.content.res.Resources
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.custom_toast.*
+import kotlinx.android.synthetic.main.custom_toast.view.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             val packageName = "com.android.settings"
             res = this.packageManager.getResourcesForApplication(packageName)
             val resourceId = res?.getIdentifier("$packageName:string/clear_activities", null, null)
-            if (resourceId != null) {
+            if (resourceId != 0 && resourceId != null) {
                 val text = this.packageManager.getText(packageName, resourceId, null)
                 Toast.makeText(this, text, Toast.LENGTH_LONG).show()
             }
@@ -30,7 +33,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getOtherAppImage() {
+        val res: Resources?
+        try {
+            val packageName = "com.hogehoge"
+            res = this.packageManager.getResourcesForApplication(packageName)
+            val resourceId = res?.getIdentifier("ic_launcher", "mipmap", packageName)
+            val drawable = resourceId?.let { ResourcesCompat.getDrawable(res, it, null) }
+            if (resourceId != 0 && resourceId != null) {
+                val toast = Toast(this)
+                toast.duration = Toast.LENGTH_LONG
+                val layout = layoutInflater.inflate(R.layout.custom_toast, linearLayout)
+                layout.customToastImage.setImageDrawable(drawable)
+                toast.view = layout
+                toast.show()
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
     fun onStringButtonClick(view: View) {
         getOtherAppString()
+    }
+
+    fun onImageButtonClick(view: View) {
+        getOtherAppImage()
     }
 }
